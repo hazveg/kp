ca7a2b56dea545a9dfe18a7e60715a81ad3d04b7
hazveg <133221043+hazveg@users.noreply.github.com>
2024-01-09 12:46:58 +0100

    started full rewrite

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
 # kp
 **Crappy CLI .kdbx reader**
 
-This was primarily made by glueing frameworks together, not that proud of it.
-I REALLY don't recommend using this, due to the fact that there's probably some horrific security concern in this.
-
-# Installation
-
-If, for whatever reason, you try using this trainwreck:
-
-## Building from source
-
-### Requirements
-- A working Rust installation (I'll refer you here: https://www.rust-lang.org/tools/install)
-- If you're on linux you'll have some compilation dependencies. Install them on Debian-based distros through
-
-```
-sudo apt install xorg-dev libxcb-composite0-dev
-```
-
-Feel free to send any PRs to fix this README, I just wanna move on to something else, man.
+Currently working on full rewrite basing off of https://github.com/Evidlo/passhole.
+Place HEAD at a5abb87 if you wanna use the old version for whatever reason.
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,27 @@
-mod file;
-mod db_interaction;
-mod string_match;
+mod argument_parsing;
 
-use db_interaction::Action;
+#[derive(PartialEq, Debug)]
+pub enum Action {
+    List,
+    Grep,
+    Add,
+    Show,
+}
 
-use keepass::DatabaseKey;
-use clap::Parser;
-use arboard::Clipboard;
-
-#[derive(Default, Parser, Debug)]
-#[clap(author="hazveg", version="0.2.0", about)]
-/// A simple KeePass database reader
-pub struct Arguments {
-    /// The path to the database file
-    path: String,
-    /// The action to be performed {list, select}
-    action: Option<String>,
-    /// Explicitly search for the given keyword instead of fuzzy matching
-    #[arg(default_value_t=false, short, long)]
-    explicit: bool,
-    /// The keyword that shall be searched for (if action is 'select')
-    keyword: Option<String>,
-    /// The password to unlock the database
-    #[arg(short, long)]
-    password: Option<String>,
+#[derive(Debug)]
+pub struct Task {
+    action: Action,
+    resource: Option<String>,
 }
 
 fn main() {
-    let args = Arguments::parse();
-    
-    let password = file::set_password(&args);
-    let database = file::unlock_database(args.path, DatabaseKey::with_password(&password));
-
-    let action = match args.action {
-        Some(action) => db_interaction::define_action(&action[..]),
-        None => Action::List
+    let task = match argument_parsing::parse_arguments(std::env::args()) {
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        },
+        Ok(task) => task,
     };
 
-    match action {
-        Action::List => db_interaction::list_entries(&database),
-        Action::Select => {
-            let mut clipboard = Clipboard::new().unwrap();
-            let entry_list = db_interaction::select_entries(&database, &args.keyword, &args.explicit);
-
-            if !db_interaction::output_selection(entry_list, &mut clipboard) {
-                eprintln!("No entries pertaining to `{}` were found", &args.keyword.unwrap());
-            }
-        },
-    }
+    dbg!(task);
 }
